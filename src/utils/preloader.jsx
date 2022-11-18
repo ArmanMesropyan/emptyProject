e3dff31ace601f21b0cc398451aa684893547cda
Arman <mesropyanarman4@gmail.com>
2022-11-18 11:19:16 +0400

    files upload

--- src/utils/preloader.jsx
+++ src/utils/preloader.jsx
@@ -7,7 +7,7 @@ const Preloader = () => {
     return isLoading && (
         <div className='P-isLoading G-flex G-flex-column G-justify-center G-align-center'>
             <CircleLoader size={75} color={'rgb(75, 148, 41)'} loading={isLoading} />
-            <p>dsfgsdfdsfdsaf </p>
+            
         </div>
     );
 };
